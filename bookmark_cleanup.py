dedd5e061ff2512ec26da2b29c2e6ce412249788
Mallikarjuna S J <msj2@users.noreply.github.com>
2017-10-31 20:33:43 +0530

    Variables outside Class or Function
    Variables outside Class or Functions must be Constants, must be in Uppercase
    
    
    https://stackoverflow.com/questions/10815549/pylint-showing-invalid-variable-name-in-output

--- bookmark_cleanup.py
+++ bookmark_cleanup.py
@@ -1,35 +1,15 @@
-#	Take FF or Chrome Bookmark htm, remove duplicates, group them....
+'''	Take FF or Chrome Bookmark htm, remove duplicates, group them....
 # any other cleanup
 #	Tue Jul 19 10:55:32 IST 2016
-
-import lxml.html
-import string
+'''
+# "(should match (([A-Z_][A-Z0-9_]*)|(__.*__))$)"
 from sys import argv
-from datetime import datetime
-
-start=datetime.now()
-script, input_file = argv
-
-full_url = input_file   
-
-image = lxml.html.parse(full_url)
-url_list = image.xpath("//a/@href")	
-
-url_list.sort()
-print "*********** All URL's before Duplicate & Sort *********** "
-#for each in url_list:
-	#print each
-print "Total no. of URL = ", len(url_list)
-
-url_list = list(set(url_list))		
-
-
-print "*********** All URL's after Duplicate & Sort *********** "
-for each in url_list:
-	print each
-print "Total no. of URL after dup remove= ", len(url_list)
-
-end=datetime.now()
-
-run_time = end - start
-print "run time = ", run_time
+import lxml.html
+LC_INPUT_FILE = argv
+FULL_URL = LC_INPUT_FILE
+IMAGE = lxml.html.parse(FULL_URL)
+URL_LIST = IMAGE.xpath("//a/@href")
+URL_LIST .sort()
+URL_LIST = list(set(URL_LIST))
+print "Total no. of URL after dup remove= ", len(URL_LIST)
+# Your code has been rated at 10.00/10 (
